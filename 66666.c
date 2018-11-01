9888cd6e8ac3f7d7f2f8ff8f6ad8b27cce79e26c
ChaYeJin1 <cha9599831@daum.net>
2018-11-01 15:34:53 +0900

    6-1

--- 66666.c
+++ 66666.c
@@ -3,28 +3,18 @@
 #define ROWS 3
 #define COLS 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void) {
-	int A[ROWS][COLS] = {//더하기 대상 행렬
-	{2, 3, 0},
-	{8, 9, 1},
-	{7, 0, 5}};
-	int B[ROWS][COLS] = {//더하기 대상 행렬
-	{1, 0, 0},
-	{0, 1, 0},
-	{0, 0, 1}};
 	int C[ROWS][COLS]; //덧셈 결과 행렬 
-	void printMatrx(int A[COLS]) {
+	void printMatrix(int A[][COLS]) {
 		int i, j;
 		for (i=0; i<ROWS; i++){
 			for (j=0; j<COLS; j++){
-				printf("d", A[i][j]);
+				printf("%d", A[i][j]);
 			}
 		printf("\n");
 		}
 		return;
 }
-	int addMatrx(int A[][COLS], int B[][COLS], int C[][COLS])
+	int addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
 	{
 		int i, j;
 		
@@ -37,3 +27,21 @@ int main(void) {
 	
 	return 1;
 }
+int main(void) {
+	
+	int A[ROWS][COLS] = {//더하기 대상 행렬
+	{2, 3, 0},
+	{8, 9, 1},
+	{7, 0, 5}};
+	int B[ROWS][COLS] = {//더하기 대상 행렬
+	{1, 0, 0},
+	{0, 1, 0},
+	{0, 0, 1}};
+	int C[ROWS][COLS];
+	addMatrix(A, B, C);
+	printMatrix(C);
+	
+	return 0;
+}
+	
+
